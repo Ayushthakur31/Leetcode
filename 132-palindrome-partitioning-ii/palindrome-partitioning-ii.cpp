ef9d8503c7a829798974801f90c385eabffa2a81
Ayushthakur31 <ayushrajthakur5@gmail.com>
2026-08-02 12:46:35 +0530

    Time: 1261 ms (51.88%) | Memory: 9.3 MB (75.72%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -25,7 +25,33 @@ public:
 //     }
 
 //memoization
-    bool palindrome(int i,int j,string &s){
+//     bool palindrome(int i,int j,string &s){
+//     while(i < j){
+//         if(s[i] != s[j]) return false;
+//         i++;
+//         j--;
+//     }
+//     return true;
+//    }
+   
+//    int f(int i,string &s,vector<int> &dp){
+//     if(i == s.size()) return 0;
+//     if(dp[i] != -1) return dp[i];
+//     int mini = 1e9;
+//     for(int j = i;j < s.size();j++){
+//         if(palindrome(i,j,s)){
+//             mini = min(mini, 1+ f(j+1,s,dp));
+//         }
+//     }
+//     return dp[i] = mini;
+//    }
+//     int minCut(string s) {
+//         vector<int> dp(s.size(),-1);
+//         return f(0,s,dp) - 1;
+//     }
+
+//Tabulation
+        bool palindrome(int i,int j,string &s){
     while(i < j){
         if(s[i] != s[j]) return false;
         i++;
@@ -33,20 +59,18 @@ public:
     }
     return true;
    }
-   
-   int f(int i,string &s,vector<int> &dp){
-    if(i == s.size()) return 0;
-    if(dp[i] != -1) return dp[i];
-    int mini = 1e9;
-    for(int j = i;j < s.size();j++){
-        if(palindrome(i,j,s)){
-            mini = min(mini, 1+ f(j+1,s,dp));
-        }
-    }
-    return dp[i] = mini;
-   }
     int minCut(string s) {
-        vector<int> dp(s.size(),-1);
-        return f(0,s,dp) - 1;
-    }
+        int n = s.size();
+        vector<int> dp(n+1,0);
+        for(int i = n-1; i>= 0;i--){
+        int mini = 1e9;
+        for(int j = i;j < s.size();j++){
+            if(palindrome(i,j,s)){
+                mini = min(mini, 1+ dp[j+1]);
+            }
+        }
+         dp[i] = mini;
+        }
+            return dp[0] - 1;
+      }    
 };
