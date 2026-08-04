3512a9b903860870e3109562f166730bdc45bbdf
Ayushthakur31 <ayushrajthakur5@gmail.com>
2026-08-04 12:27:33 +0530

    Time: 10 ms (7.46%) | Memory: 33.6 MB (28.39%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        int min = *min_element(nums.begin(),nums.end());
+        int max = *max_element(nums.begin(),nums.end());
+        vector<int> ans;
+        unordered_set<int> st(nums.begin(),nums.end());
+        for(int i = min;i <= max;i++){
+            if(!st.count(i)){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
