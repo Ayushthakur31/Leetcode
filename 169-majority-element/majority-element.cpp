3e7ecce602a02403bf99f86d2df732fbc6fbe5de
Ayushthakur31 <ayushrajthakur5@gmail.com>
2026-08-05 12:49:04 +0530

    Time: 4 ms (19.01%) | Memory: 28.3 MB (40.36%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        int freq = 0;
+        int ans = nums[0];
+        for(int i = 1;i < nums.size();i++){
+            if(nums[i] == nums[i-1]){
+                freq++;
+            }
+            else{
+                freq = 1;
+            }
+            ans = nums[i];
+            if(freq > n/2) return ans;
+        }
+        return nums[0];
+    }
+};
