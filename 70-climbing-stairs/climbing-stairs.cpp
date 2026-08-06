b0f6497290a449321beba1d668f7f75363bcafc3
Ayushthakur31 <ayushrajthakur5@gmail.com>
2026-08-06 15:48:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (20.81%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -10,13 +10,25 @@ public:
     // }
 
 //Memoization
-     int f(int n,vector<int> &dp){
-        if(n <= 2) return n;
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = f(n-1,dp) + f(n-2,dp);
-    }
+    //  int f(int n,vector<int> &dp){
+    //     if(n <= 2) return n;
+    //     if(dp[n] != -1) return dp[n];
+    //     return dp[n] = f(n-1,dp) + f(n-2,dp);
+    // }
+    // int climbStairs(int n) {
+    //     vector<int> dp(n+1,-1);
+    //     return f(n,dp);
+    // }
+
+//Tabulation
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return f(n,dp);
-    }    
+        if(n <= 2) return n;
+        vector<int> dp(n+1,0);
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3;i <= n;i++){
+             dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }      
 };
