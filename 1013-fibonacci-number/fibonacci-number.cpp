0ad744ed1260f2630ec9ab35cbb2aa1649095f87
Ayushthakur31 <ayushrajthakur5@gmail.com>
2026-08-07 15:42:14 +0530

    Time: 2 ms (77.79%) | Memory: 7.7 MB (65.83%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -23,15 +23,29 @@ public:
 //     }
 
 //Tabulation
-   int fib(int n) {
+//    int fib(int n) {
+//          if (n <= 1)
+//            return n; 
+//         vector<int> dp(n+1,0);
+//         dp[0] = 0;
+//         dp[1] = 1;
+//         for(int i = 2;i <= n;i++){  
+//            dp[i] = dp[i-1] + dp[i-2];
+//         }
+//        return dp[n];
+//    }
+
+//Space Optimisation
+    int fib(int n) {
          if (n <= 1)
            return n; 
-        vector<int> dp(n+1,0);
-        dp[0] = 0;
-        dp[1] = 1;
+        int prev1 = 0;
+        int prev2 = 1;
         for(int i = 2;i <= n;i++){  
-           dp[i] = dp[i-1] + dp[i-2];
+           int curr = prev1 + prev2;
+           prev1 = prev2;
+           prev2 = curr;
         }
-       return dp[n];
+       return prev2;
    }
 };
