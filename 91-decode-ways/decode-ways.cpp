39203ea53a8ea8ac2a75a01c3dd0b6cc8f4b030f
Ayushthakur31 <ayushrajthakur5@gmail.com>
2026-08-08 12:47:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (80.28%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -19,22 +19,43 @@ public:
     // }
 
 //Memoisation
-    int f(int i,string& s,vector<int>& dp){
-        if(i == s.size()) return 1;
-        if(s[i] == '0') return 0;
-        if(dp[i] != -1) return dp[i];
-        int ans = f(i+1,s,dp);
+    // int f(int i,string& s,vector<int>& dp){
+    //     if(i == s.size()) return 1;
+    //     if(s[i] == '0') return 0;
+    //     if(dp[i] != -1) return dp[i];
+    //     int ans = f(i+1,s,dp);
 
-        if(i+1 < s.size()){
-            int num = (s[i] - '0') * 10 + s[i+1] - '0';
-        if(num >=10 && num <= 26)
-        ans += f(i+2,s,dp);
-        }
-        return dp[i] = ans;
-    }
+    //     if(i+1 < s.size()){
+    //         int num = (s[i] - '0') * 10 + s[i+1] - '0';
+    //     if(num >=10 && num <= 26)
+    //     ans += f(i+2,s,dp);
+    //     }
+    //     return dp[i] = ans;
+    // }
+    // int numDecodings(string s) {
+    //     int n = s.size();
+    //     vector<int> dp(n+1,-1);
+    //     return f(0,s,dp);
+    // }  
+
+//Tabulation
     int numDecodings(string s) {
         int n = s.size();
-        vector<int> dp(n+1,-1);
-        return f(0,s,dp);
-    }    
+        vector<int> dp(n+1,0);
+        dp[n] = 1;
+        for(int i = n-1;i >= 0;i--){
+            if(s[i] == '0') {
+            dp[i] = 0;
+            continue;
+}
+         dp[i] = dp[i+1];
+
+        if(i+1 < n){
+            int num = (s[i] - '0') * 10 + s[i+1] - '0';
+        if(num >=10 && num <= 26)
+        dp[i] += dp[i+2];
+        }
+        }
+        return dp[0];
+    }      
 };
