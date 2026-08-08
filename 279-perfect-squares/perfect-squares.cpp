90b4aa5af097b367dd41f5ae7665896887f356cc
Ayushthakur31 <ayushrajthakur5@gmail.com>
2026-08-08 13:15:42 +0530

    Time: 83 ms (33.29%) | Memory: 13.9 MB (39.37%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+//Recursion
+    // int f(int n){
+    //     if(n == 0) return 0;
+    //     int ans = 1e9;
+
+    //     for(int i = 1;i * i <= n;i++){
+    //         int square = i*i;
+    //         ans = min(ans, 1+f(n - square));
+    //     }
+    //     return ans;
+    // }
+    // int numSquares(int n) {
+    //     return f(n);
+    // }
+
+//Memoisation
+    int f(int n,vector<int>& dp){
+        if(n == 0) return 0;
+        if(dp[n] != -1) return dp[n];
+        int ans = 1e9;
+
+        for(int i = 1;i * i <= n;i++){
+            int square = i*i;
+            ans = min(ans, 1+f(n - square,dp));
+        }
+        return dp[n] = ans;
+    }
+    int numSquares(int n) {
+        vector<int> dp(n+1,-1);
+        return f(n,dp);
+    }    
+};
