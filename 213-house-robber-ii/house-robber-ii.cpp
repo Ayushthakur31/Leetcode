7931e2fa03c32bfe237bcff572111638f0025c32
Ayushthakur31 <ayushrajthakur5@gmail.com>
2026-08-08 10:52:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (38.16%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+//Recursion
+//     int f(int i,int n,vector<int>& nums){
+//         if(i >= n) return 0;
+//         int pick = nums[i] + f(i+2,n,nums);
+//         int notpick = f(i+1,n,nums);
+//         return max(pick,notpick);
+//     }
+//    int rob(vector<int>& nums) {
+//         int n = nums.size();
+//         if(n == 1) return nums[0];
+
+//         int case1 = f(0,n-1,nums);
+//         int case2 = f(1,n,nums);
+//         return max(case1,case2);
+//     }
+
+//Memoization
+    	  int f(int i,int n,vector<int>& nums,vector<int>& dp){
+        if(i >= n) return 0;
+        if(dp[i] != -1) return dp[i];
+        int pick = nums[i] + f(i+2,n,nums,dp);
+        int notpick = f(i+1,n,nums,dp);
+        return dp[i] = max(pick,notpick);
+    }
+   int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        vector<int> dp1(n+1,-1);
+        int case1 = f(0,n-1,nums,dp1);
+        vector<int> dp2(n+1,-1);
+        int case2 = f(1,n,nums,dp2);
+        return max(case1,case2);
+    }
+};
