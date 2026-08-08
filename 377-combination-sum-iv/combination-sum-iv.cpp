af2327a43480bf029c0d809490b35808456a5443
Ayushthakur31 <ayushrajthakur5@gmail.com>
2026-08-08 11:40:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (52.47%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,32 +1,64 @@
-class Solution {
-public:
-//Recursion
-    // int f(vector<int>& nums,int target){
-    //     if(target == 0) return 1;
-    //     if(target < 0) return 0;
-    //     int ans = 0;
-    //     for(int i = 0;i < nums.size();i++){
-    //         ans += f(nums,target-nums[i]);
-    //     }
-    //     return ans;
-    // }
-    // int combinationSum4(vector<int>& nums, int target) {
-    //     return f(nums,target);
-    // }
+    class Solution {
+    public:
+    //Recursion
+        // int f(vector<int>& nums,int target){
+        //     if(target == 0) return 1;
+        //     if(target < 0) return 0;
+        //     int ans = 0;
+        //     for(int i = 0;i < nums.size();i++){
+        //         ans += f(nums,target-nums[i]);
+        //     }
+        //     return ans;
+        // }
+        // int combinationSum4(vector<int>& nums, int target) {
+        //     return f(nums,target);
+        // }
 
-//Memoisation
-    int f(vector<int>& nums,int target,vector<int>& dp){
-        if(target == 0) return 1;
-        if(target < 0) return 0;
-        if(dp[target] != -1) return dp[target];
-        int ans = 0;
-        for(int i = 0;i < nums.size();i++){
-            ans += f(nums,target-nums[i],dp);
-        }
-        return dp[target] = ans;
-    }
+    //Memoisation
+        // int f(vector<int>& nums,int target,vector<int>& dp){
+        //     if(target == 0) return 1;
+        //     if(target < 0) return 0;
+        //     if(dp[target] != -1) return dp[target];
+        //     int ans = 0;
+        //     for(int i = 0;i < nums.size();i++){
+        //         ans += f(nums,target-nums[i],dp);
+        //     }
+        //     return dp[target] = ans;
+        // }
+        // int combinationSum4(vector<int>& nums, int target) {
+        //     vector<int> dp(target+1,-1);
+        //     return f(nums,target,dp);
+        // }   
+
+    //Tabulation
+        // int combinationSum4(vector<int>& nums, int target) {
+        //     vector<int> dp(target+1,0);
+        //     dp[0] = 1;
+        //     for(int j = 1;j <= target;j++){
+        //     int ans = 0;
+        //     for(int i = 0;i < nums.size();i++){
+        //         if(nums[i] <= j)
+        //         ans += dp[j - nums[i]];
+        //     }
+        //     dp[j] = ans;
+        //     }
+        //     return dp[target];
+        // }     
+
+    //space optimisation
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int> dp(target+1,-1);
-        return f(nums,target,dp);
-    }    
-};
+        vector<double> dp(target + 1, 0);
+
+        dp[0] = 1;
+
+        for(int j = 1; j <= target; j++) {
+            for(int i = 0; i < nums.size(); i++) {
+                if(nums[i] <= j) {
+                    dp[j] += dp[j - nums[i]];
+                }
+            }
+        }
+
+        return (int)dp[target];
+    }
+    };
