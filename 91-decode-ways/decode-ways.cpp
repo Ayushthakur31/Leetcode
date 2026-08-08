97bc1a7e02aa3d133834ae5f1d5643495bd84c86
Ayushthakur31 <ayushrajthakur5@gmail.com>
2026-08-08 12:40:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (46.25%) - LeetSync

---
+++ 91-decode-ways/decode-ways.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+//Recursion
+    // int f(int i,string& s){
+    //     if(i == s.size()) return 1;
+    //     if(s[i] == '0') return 0;
+
+    //     int ans = f(i+1,s);
+
+    //     if(i+1 < s.size()){
+    //         int num = (s[i] - '0') * 10 + s[i+1] - '0';
+    //     if(num >=10 && num <= 26)
+    //     ans += f(i+2,s);
+    //     }
+    //     return ans;
+    // }
+    // int numDecodings(string s) {
+    //     return f(0,s);
+    // }
+
+//Memoisation
+    int f(int i,string& s,vector<int>& dp){
+        if(i == s.size()) return 1;
+        if(s[i] == '0') return 0;
+        if(dp[i] != -1) return dp[i];
+        int ans = f(i+1,s,dp);
+
+        if(i+1 < s.size()){
+            int num = (s[i] - '0') * 10 + s[i+1] - '0';
+        if(num >=10 && num <= 26)
+        ans += f(i+2,s,dp);
+        }
+        return dp[i] = ans;
+    }
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int> dp(n+1,-1);
+        return f(0,s,dp);
+    }    
+};
