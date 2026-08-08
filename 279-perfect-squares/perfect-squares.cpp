c47d429031bffdab8d2c7a9e963443ac365e0e93
Ayushthakur31 <ayushrajthakur5@gmail.com>
2026-08-08 13:21:48 +0530

    Time: 29 ms (92.96%) | Memory: 13.2 MB (57.26%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -16,19 +16,35 @@ public:
     // }
 
 //Memoisation
-    int f(int n,vector<int>& dp){
-        if(n == 0) return 0;
-        if(dp[n] != -1) return dp[n];
+    // int f(int n,vector<int>& dp){
+    //     if(n == 0) return 0;
+    //     if(dp[n] != -1) return dp[n];
+    //     int ans = 1e9;
+
+    //     for(int i = 1;i * i <= n;i++){
+    //         int square = i*i;
+    //         ans = min(ans, 1+f(n - square,dp));
+    //     }
+    //     return dp[n] = ans;
+    // }
+    // int numSquares(int n) {
+    //     vector<int> dp(n+1,-1);
+    //     return f(n,dp);
+    // }  
+
+//Tabulation
+    int numSquares(int n) {
+        vector<int> dp(n+1,0);
+          dp[0] = 0;
+        for(int i = 1;i <= n;i++){
         int ans = 1e9;
 
-        for(int i = 1;i * i <= n;i++){
-            int square = i*i;
-            ans = min(ans, 1+f(n - square,dp));
+        for(int j = 1;j * j <= i;j++){
+            int square = j*j;
+            ans = min(ans, 1+dp[i - square]);
         }
-        return dp[n] = ans;
-    }
-    int numSquares(int n) {
-        vector<int> dp(n+1,-1);
-        return f(n,dp);
-    }    
+          dp[i] = ans;
+        }
+        return dp[n];
+    }      
 };
