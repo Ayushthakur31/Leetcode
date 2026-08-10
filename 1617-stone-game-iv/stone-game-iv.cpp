e8e506422beb6288fccf15acd10a9c2444673db0
Ayushthakur31 <ayushrajthakur5@gmail.com>
2026-08-10 18:26:28 +0530

    Time: 15 ms (95.36%) | Memory: 10.8 MB (70.40%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.cpp
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -13,16 +13,31 @@ public:
     // }
 
 //Memoisation
-      bool f(int n,vector<int> &dp){
-        if( n == 0) return 0;
-        if(dp[n] != -1) return dp[n];
-        for(int i = 1;i * i <= n;i++){
-            if(!f(n-i*i,dp)) return dp[n] =  true;
+    //   bool f(int n,vector<int> &dp){
+    //     if( n == 0) return 0;
+    //     if(dp[n] != -1) return dp[n];
+    //     for(int i = 1;i * i <= n;i++){
+    //         if(!f(n-i*i,dp)) return dp[n] =  true;
+    //     }
+    //     return dp[n] = false;
+    // }
+    // bool winnerSquareGame(int n) {
+    //     vector<int> dp(n+1,-1);
+    //     return f(n,dp);
+    // } 
+
+//Tabulation
+     bool winnerSquareGame(int n) {
+        vector<int> dp(n+1,false);
+        dp[0] = false;
+        for(int j = 1;j <= n;j++){
+        for(int i = 1;i * i <= j;i++){
+            if(!dp[j - i*i]) {
+                dp[j] =  true;
+                break;
         }
-        return dp[n] = false;
-    }
-    bool winnerSquareGame(int n) {
-        vector<int> dp(n+1,-1);
-        return f(n,dp);
-    }    
+        }
+        }
+        return dp[n];
+    }        
 };
