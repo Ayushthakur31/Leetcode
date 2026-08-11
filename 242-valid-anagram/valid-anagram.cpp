8ad7a06013b55d2851efaea0576ae54605fed93d
Ayushthakur31 <ayushrajthakur5@gmail.com>
2026-08-11 23:01:25 +0530

    Time: 2 ms (50.10%) | Memory: 9.8 MB (50.00%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       if(s.size() != t.size()) return false;
+       unordered_map<char,int> mpp;
+       for(auto c : s){
+         mpp[c] ++;
+       }
+       for(auto c : t){
+        if(mpp[c] == 0) return false;
+        mpp[c]--;
+       }
+       return true;
+    }
+};
