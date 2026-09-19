842404717ab0670698c8b1dc2133a88dbeaaa525
Ayushthakur31 <ayushrajthakur5@gmail.com>
2026-09-19 12:45:48 +0530

    Time: 6 ms (86.99%) | Memory: 23.4 MB (71.57%) - LeetSync

---
+++ 1644-maximum-number-of-non-overlapping-substrings/maximum-number-of-non-overlapping-substrings.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<string> maxNumOfSubstrings(string s) {
+        int n = s.size();
+        vector<int> first(26, n), last(26, -1);
+
+        for (int i = 0; i < n; ++i) {
+            int c = s[i] - 'a';
+            first[c] = min(first[c], i);
+            last[c] = i;
+        }
+
+        vector<string> ans;
+        int prevEnd = -1;
+
+        for (int i = 0; i < n; ++i) {
+            int c = s[i] - 'a';
+
+            if (i != first[c])
+                continue;
+
+            int end = last[c];
+            bool valid = true;
+
+            for (int j = i; j <= end; ++j) {
+                int x = s[j] - 'a';
+
+                if (first[x] < i) {
+                    valid = false;
+                    break;
+                }
+
+                end = max(end, last[x]);
+            }
+
+            if (!valid)
+                continue;
+
+            if (i > prevEnd) {
+                ans.push_back(s.substr(i, end - i + 1));
+            } else {
+                ans.back() = s.substr(i, end - i + 1);
+            }
+
+            prevEnd = end;
+        }
+
+        return ans;
+    }
+};
